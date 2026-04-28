2b8c2e1c6f05bb770fff30d9df6adc0457d8bc4b
Student <student@example.com>
2026-04-28 16:25:14 +0530

    Copy final_practical1 and final_practical2 into final_EST (resolve file locks)

---
+++ final_EST/final_practical2/src/Header.jsx
@@ -0,0 +1,12 @@
+import Icon from "./Icon";
+
+function Header({ username }) {
+  return (
+    <div>
+      <h2>Header Component</h2>
+      <Icon username={username} />
+    </div>
+  );
+}
+
+export default Header;
